404f98865ff087cf14f79307315541708cd8b413
kevin <bridget.nmaclou@gmail.com>
2025-02-22 12:01:31 +0200

    afterclass project

---
+++ afterclass.py
@@ -0,0 +1,20 @@
+name1= "Kaylim Taylor"
+bday1= "22nd April"
+print(f"{bday1} - {name1}")
+
+name2= "Musa"
+bday2= "21st May"
+print(f"{bday2} - {name2}")
+
+name3= "Rogan"
+bday3= "12 November"
+print(f"{bday3} - {name3}")
+
+name4= "Sithe"
+bday4= "11th April"
+print(f"{bday4} - {name4}")
+
+name5= "Elona"
+bday5= "17 January"
+print(f"{bday5} - {name5}")
+
