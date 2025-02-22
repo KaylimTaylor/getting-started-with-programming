dd9a5050bd91706945ee1ec4354882266c0e93e2
kevin <bridget.nmaclou@gmail.com>
2025-02-22 11:14:26 +0200

    first commit

---
+++ variables.py
@@ -0,0 +1,10 @@
+name = "Kaylim T.aylor"
+age = 13
+role = "soccer player"
+
+print(f"My name is {name}")
+print(f"My age is {age}")
+print(f"i am a well known {role}")
+
+match=input("which team do you want me to play next:")
+print(f"okay i will play my next match against {match}")
---
+++ welcome.py
@@ -0,0 +1,5 @@
+print("Welcome world. My name is Kaylim.")
+
+print("my age is ",10+3)
+
+print(f"Welcome world.My name is Kaylim Taylor.\nMy age is {10+3}")
